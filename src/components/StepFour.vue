587a950af49b98e473e7ee72764b688ab9d0bf95
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-10-21 09:35:37 +0200

    placeholder app file

---
+++ src/components/StepFour.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h2>Step 4: Placeholder content</h2>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    setup() {
+      const placeholder = ref('Step 4 content goes here');
+      return {
+        placeholder
+      };
+    }
+  };
+  </script>
+  
